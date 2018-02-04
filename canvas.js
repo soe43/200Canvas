1bde568a552d41630de4c9bbfb597e91b6a9a13f
soe43 <soe433@gmail.com>
2018-02-04 11:16:40 -0500

    finished the work

--- canvas.js
+++ canvas.js
@@ -1,18 +1,51 @@
-var ctx = document.getElementbyID("board");
-var clearbutt = document.getElementbyID("clear");
-var togglebutt = document.getElementbyID("toggle");
+var board=document.getElementById("board");
+var ctx=board.getContext("2d");
+var clearbutt=document.getElementById("clear");
+var togglebutt=document.getElementById("toggle");
 
-var drawShape = 'circle';
+var drawShape = 'square';
 
 var drawRect = function(e){
-    mouseX = e.pageX;
-    mouseY = e.page;
+    var mouseX = e.pageX - 35;
+    var mouseY = e.pageY - 35;
+    ctx.fillStyle = "lightsteelblue";
+    ctx.fillRect(mouseX, mouseY, 50, 50);
 };
 
 var drawCircle = function(e){
-
+    var mouseX = e.clientX - 11;
+    var mouseY = e.clientY - 11;
+    ctx.fillStyle = "lightsteelblue";
+    ctx.beginPath()
+    ctx.arc(mouseX, mouseY, 25, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
 };
 
-var clear = function(e){
+var clear = function(){
     ctx.clearRect(0,0,500,500);
 };
+
+var switchShapes = function(){
+    if(drawShape == 'circle'){
+	drawShape = 'square';
+    }
+    else{
+	drawShape = 'circle';
+    }
+    console.log(drawShape)
+};
+
+var addShape = function(e){
+    if(drawShape == 'circle'){
+	drawCircle(e);
+    }
+    else{
+	drawRect(e);
+    }
+}
+
+board.addEventListener("click", addShape);
+clearbutt.addEventListener("click", clear);
+togglebutt.addEventListener("click", switchShapes);
+
